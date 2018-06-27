8c78b34302a1ecb79c39a8d3330af7c76d06f99b
Alisa Feng <feng.alisa@yahoo.com>
2018-06-27 20:37:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,10 +36,11 @@ function sayHiToGrandma(string) {
     return 'YES INDEED!'
   }
   
-  if (string.toLowerCase()===lowercase) {
-    return "I can\'t hear you!"
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
   }
-  else if ((string)==="I love you, Grandma.") {
+  
+  if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
 }
